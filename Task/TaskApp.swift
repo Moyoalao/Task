c1a088474d4787cf7f56941e48cd877e79eb01b6
Musibau Moyosore Alao <73694764+Moyoalao@users.noreply.github.com>
2024-10-22 10:58:48 +0100

    Initial Commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Musibau Alao on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskTests/TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTests.swift
+//  TaskTests
+//
+//  Created by Musibau Alao on 22/10/2024.
+//
+
+import Testing
+@testable import Task
+
+struct TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
